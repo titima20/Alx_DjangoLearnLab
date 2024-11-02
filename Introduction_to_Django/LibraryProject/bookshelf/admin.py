bed92fcf9bde07a89c0cf234024967ebe043488a
Fatima Ezzahraa Elouidad <titimaelouidad@gmail.com>
2024-11-02 16:21:02 +0100

    Add Book model import to delete.md

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    list_filter = ('author',)
+    search_fields = ('title', 'author')
+
+admin.site.register(Book, BookAdmin)
