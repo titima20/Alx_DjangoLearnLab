189f41f667815fd9f8e735f1b8d89cbfd96ee303
Fatima Ezzahraa Elouidad <titimaelouidad@gmail.com>
2024-11-17 12:44:33 +0100

    library_details.html added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -15,6 +15,15 @@ from django.contrib.auth.mixins import UserPassesTestMixin, PermissionRequiredMi
 from .models import Author, Book, Librarian, Library, UserProfile
 from .forms import BookForm
 
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        return context
 
 # Views for listing and displaying models
 def booklist(request):
