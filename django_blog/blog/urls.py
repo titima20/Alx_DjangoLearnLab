e7161c414623f4847a78d1feef9fe85b6a0dcd1f
Fatima Ezzahraa Elouidad <titimaelouidad@gmail.com>
2024-12-05 18:54:44 +0100

    Add intuitive URL patterns for comment creation and updates

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
-    path('post/<int:pk>/comment/', views.add_comment, name='add_comment'),
-    path('comment/<int:pk>/edit/', views.edit_comment, name='edit_comment'),
-    path('comment/<int:pk>/delete/', views.delete_comment, name='delete_comment'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
 ]
