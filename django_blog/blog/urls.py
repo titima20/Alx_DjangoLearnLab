9d85d2d5538960ef93d8590258958ffb8e5d00e8
Fatima Ezzahraa Elouidad <titimaelouidad@gmail.com>
2024-12-05 13:28:09 +0100

    Add login URL pattern

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'blog'
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
 ]
