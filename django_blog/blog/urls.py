1cd4ee278f36105e3ea2bf4f7d36c5361baaebec
Fatima Ezzahraa Elouidad <titimaelouidad@gmail.com>
2024-12-05 18:55:34 +0100

    Add comment delete URL pattern

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
     path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
