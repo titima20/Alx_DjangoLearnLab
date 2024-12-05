a7adff6e87b31a8c3b47c0be948f3bbf060d8bbd
Fatima Ezzahraa Elouidad <titimaelouidad@gmail.com>
2024-12-05 13:27:03 +0100

    Add authentication URLs and forms

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
     path('', include('django.contrib.auth.urls')),
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
 ]
 
