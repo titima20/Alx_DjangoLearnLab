72ded710db6a03bd9fd95d9292db762946c1e164
Fatima Ezzahraa Elouidad <titimaelouidad@gmail.com>
2024-12-12 14:26:50 +0100

    Add explicit CharField definitions in UserSerializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -9,10 +9,11 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, style={'input_type': 'password'})
     confirm_password = serializers.CharField(write_only=True, style={'input_type': 'password'})
     bio = serializers.CharField(required=False, allow_blank=True)
+    token = serializers.CharField(read_only=True)
 
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'password', 'confirm_password', 'bio', 'profile_picture')
+        fields = ('id', 'username', 'email', 'password', 'confirm_password', 'bio', 'profile_picture', 'token')
 
     def validate(self, data):
         if data['password'] != data['confirm_password']:
@@ -24,4 +25,3 @@ class UserSerializer(serializers.ModelSerializer):
         user = get_user_model().objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
-
