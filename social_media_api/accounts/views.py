c9e802beb1ae906226f474899fa2142dea6b1637
Fatima Ezzahraa Elouidad <titimaelouidad@gmail.com>
2024-12-12 14:32:38 +0100

    Complete implementation of social_media_api with user authentication

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
