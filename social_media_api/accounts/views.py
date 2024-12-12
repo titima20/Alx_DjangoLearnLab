edcbdfffd0bae93395296521c3724f600b34e556
Fatima Ezzahraa Elouidad <titimaelouidad@gmail.com>
2024-12-12 15:39:28 +0100

    Add GenericAPIView and IsAuthenticated to views

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,8 +4,10 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import authenticate
+from django.shortcuts import get_object_or_404
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
+from .models import CustomUser
 
 
 @api_view(['POST'])
@@ -46,6 +48,28 @@ def follow_user(request, user_id):
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+    request.user.following.remove(user_to_unfollow)
+    return Response({'status': 'unfollowed'}, status=status.HTTP_200_OK)
+
+
+class UserListView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(CustomUser, id=user_id)
+    if request.user != user_to_follow:
+        request.user.following.add(user_to_follow)
+        return Response({'status': 'following'}, status=status.HTTP_200_OK)
+    return Response({'error': 'Cannot follow yourself'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
 def unfollow_user(request, user_id):
     user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
     request.user.following.remove(user_to_unfollow)
